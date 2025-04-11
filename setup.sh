a4bf1df4dbc4381933a271b80353bba58b598d41
DStener <stener@internet.ru>
2025-04-11 22:22:41 +0500

    New chenge

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ export CXX="$(which clang++)"
 
 
 CMAKE_CONFIG_ARGS=""
+CMAKE_BUILD_ARGS=""
 
 if [ -d "$1" ]; then
 	cd $1
@@ -14,6 +15,8 @@ fi
 
 if [[ $* == *--update* ]]; then 
 	CMAKE_CONFIG_ARGS="--fresh"
+	CMAKE_BUILD_ARGS="--clean-first"
+
 	git fetch --all
 	git reset --hard origin/main
 	git clean
@@ -25,7 +28,7 @@ fi
 # Configure project
 cmake -B build ${CMAKE_CONFIG_ARGS} -DCMAKE_BUILD_TYPE=Release .
 cd ./build
-cmake --build . -j $(nproc) --config Release
+cmake --build . ${CMAKE_BUILD_ARGS} -j $(nproc) --config Release
 
 if [[ $* == *--run* ]]; then
 	./logmat
